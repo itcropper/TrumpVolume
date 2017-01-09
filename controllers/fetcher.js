f354352279951b2950d73c82802dcc927f12ba03
Ian Cropper <icropper@homeaway.com>
2017-01-08 21:47:18 -0700

    fixed another issue with the date

--- controllers/fetcher.js
+++ controllers/fetcher.js
@@ -89,7 +89,7 @@ function getDisplay(render, sorter = "count"){
         var day = 1000 * 60 * 60 * 24;
         var d = data.map((m) => {
             var time = m.instances[0].time,
-                daySpan = (months[time.getMonth() + 1])+ " "+ (time.getDate() - 1) + ", " + time.getFullYear();
+                daySpan = (months[time.getMonth() + 1])+ " "+ (time.getDate()) + ", " + time.getFullYear();
             return {
                 src: m._id,
                 name: m.sourceName,
